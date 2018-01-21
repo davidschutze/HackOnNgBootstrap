2b4377e3e2893218430c0a2b9954ab4486ba3852
David Schutze <davidschutze@gmail.com>
2018-01-21 13:59:34 -0500

    removes relative path from images

--- src/app/slide.service.ts
+++ src/app/slide.service.ts
@@ -22,29 +22,29 @@ export class SlideService {
 const SLIDES: ISlide[] = [
     {
         id: 1,
-        mainImage: '/assets/lorempixel-1.jpg',
-        smallImage: '/assets/lorempixel-1sm.jpg',
+        mainImage: 'assets/lorempixel-1.jpg',
+        smallImage: 'assets/lorempixel-1sm.jpg',
         title: 'I can see you want that cheeseburger over there.',
         subtitle: "It's mine just as soon as I can get this camera out of my eye."
     },
     {
         id: 1,
-        mainImage: '/assets/lorempixel-2.jpg',
-        smallImage: '/assets/lorempixel-2sm.jpg',
+        mainImage: 'assets/lorempixel-2.jpg',
+        smallImage: 'assets/lorempixel-2sm.jpg',
         title: "Please don't make me cry.",
         subtitle: "I love cheeseburgers. I need that cheeseburger."
     },
     {
         id: 1,
-        mainImage: '/assets/lorempixel-3.jpg',
-        smallImage: '/assets/lorempixel-3sm.jpg',
+        mainImage: 'assets/lorempixel-3.jpg',
+        smallImage: 'assets/lorempixel-3sm.jpg',
         title: 'You want that cheeseburger?',
         subtitle: 'Ask yourself one question... "Do I feel lucky?" Well, do you ya?'
     },
     {
         id: 1,
-        mainImage: '/assets/lorempixel-4.jpg',
-        smallImage: '/assets/lorempixel-4sm.jpg',
+        mainImage: 'assets/lorempixel-4.jpg',
+        smallImage: 'assets/lorempixel-4sm.jpg',
         title: 'Mmmmm...Cheeseburger!',
         subtitle: 'Go ahead, have a cheeseburger.'
     }
